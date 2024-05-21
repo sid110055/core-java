15a23d23251243b54ab585270a802e7591e6617d
Siddhartha Chikkavarapu <siddu_chikkavarapu@Siddharthas-MacBook-Pro.local>
2024-05-21 16:55:12 -0500

    working on the collection framework

---
+++ app/src/main/java/org/prasquare/collection_framework/WorkingWithList.java
@@ -0,0 +1,11 @@
+package org.prasquare.collection_framework;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class WorkingWithList {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("t", "e", "s", "t");
+        System.out.println(list);
+    }
+}
