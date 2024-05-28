15ae14789e5fdd33876c4f9040a94926bca3d20e
Prasanna <nvprasanna191998@gmail.com>
2024-05-28 12:04:38 -0500

    "Initial commit"

---
+++ src/main/java/org/example/practiceVariables.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class practiceVariables {
+    public static void main(String[] args){
+      int a = 34;
+String s = Integer.toString(a);
+        System.out.println(s  + 2);
+
+    }
+    }
+
