6ff781b3db19e56e21018cd908964cdfde9122d5
Jhansi089 <jhansi.garneni@gmail.com>
2024-05-29 08:55:23 -0700

    Add files via upload

---
+++ Narrowcast.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class Narrowcast {
+	public static void main(String[] args) {
+		double mydouble = 8.96d;
+		int myint = (int) mydouble;
+		
+		System.out.println(mydouble);
+		System.out.println(myint);
+	}
+
+}
