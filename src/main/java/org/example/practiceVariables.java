65faf6610315e3680a75a4d54abfc46b133ea7fa
nvprasanna1998 <135443271+nvprasanna1998@users.noreply.github.com>
2024-06-03 16:20:56 -0500

    InitialCommit

--- src/main/java/org/example/practiceVariables.java
+++ src/main/java/org/example/practiceVariables.java
@@ -4,7 +4,7 @@ public class practiceVariables {
     public static void main(String[] args){
       int a = 34;
 String s = Integer.toString(a);
-        System.out.println(s  + 3);
+        System.out.println(s  + 1);
 
     }
     }
